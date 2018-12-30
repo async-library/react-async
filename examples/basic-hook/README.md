14268c5173591e0a5d478c105a5e945113554f19
Gert Hengeveld <info@ghengeveld.nl>
2018-12-30 23:05:17 +0100

    Add basic useAsync example.

---
+++ examples/basic-hook/README.md
@@ -0,0 +1,5 @@
+[![Deploy to now](https://deploy.now.sh/static/button.svg)](https://deploy.now.sh/?repo=https://github.com/ghengeveld/react-async/tree/master/examples/basic-hook)
+
+# Basic fetch with useAsync hook
+
+This demonstrates how to use the `useAsync` hook.
---
+++ examples/basic-hook/src/index.js
@@ -0,0 +1,43 @@
+import React from "react"
+import { useAsync } from "react-async"
+import ReactDOM from "react-dom"
+import "./index.css"
+
+const loadUser = ({ userId }) =>
+  fetch(`https://reqres.in/api/users/${userId}`)
+    .then(res => (res.ok ? res : Promise.reject(res)))
+    .then(res => res.json())
+    .then(({ data }) => data)
+
+const UserPlaceholder = () => (
+  <div className="user placeholder">
+    <div className="avatar" />
+    <div className="name">══════</div>
+  </div>
+)
+
+const UserDetails = ({ data }) => (
+  <div className="user">
+    <img className="avatar" src={data.avatar} alt="" />
+    <div className="name">
+      {data.first_name} {data.last_name}
+    </div>
+  </div>
+)
+
+const User = ({ userId }) => {
+  const { data, error, isLoading } = useAsync({ promiseFn: loadUser, userId })
+  if (isLoading) return <UserPlaceholder />
+  if (error) return <p>{error.message}</p>
+  if (data) return <UserDetails data={data} />
+  return null
+}
+
+const App = () => (
+  <>
+    <User userId={1} />
+    <User userId={2} />
+  </>
+)
+
+ReactDOM.render(<App />, document.getElementById("root"))
