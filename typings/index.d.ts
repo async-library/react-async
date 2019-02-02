1c79637e4a6ab1eefc572bd9a7a3a8565d3cca15
Jiří Brabec <brabeji@gmail.com>
2019-02-02 14:20:52 +0100

    Fix AsyncProps type (#19)

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -3,7 +3,7 @@ import * as React from "react"
 type AsyncChildren<T> = ((state: AsyncState<T>) => React.ReactNode) | React.ReactNode
 type PromiseFn<T> = (props: object) => Promise<T>
 
-interface AsyncProps<T> {
+interface AsyncOptions<T> {
   promiseFn?: (props: object, controller: AbortController) => Promise<T>
   deferFn?: (...args: any[]) => Promise<T>
   watch?: any
