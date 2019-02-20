45659ed56616fc07d3f4b9a7b3897323039d74c1
Gert Hengeveld <info@ghengeveld.nl>
2019-02-20 09:29:44 +0100

    Import from the index to make sure it exports things correctly.

--- src/useAsync.spec.js
+++ src/useAsync.spec.js
@@ -1,7 +1,7 @@
 import "jest-dom/extend-expect"
 import React from "react"
 import { render, fireEvent, cleanup, waitForElement } from "react-testing-library"
-import { useAsync, useFetch } from "."
+import { useAsync, useFetch } from "./index"
 
 const abortCtrl = { abort: jest.fn(), signal: "SIGNAL" }
 window.AbortController = jest.fn(() => abortCtrl)
