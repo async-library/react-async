7004fd92faa53f5f10cdbca7c969a82efc3dcd13
Gert Hengeveld <info@ghengeveld.nl>
2019-07-05 09:02:24 +0200

    Remove @babel/preset-env because it leads to broken builds.

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@
 console.log("Loaded babel.config.js")
 
 module.exports = {
-  presets: ["@babel/preset-env", "@babel/preset-react"],
+  presets: ["@babel/preset-react"],
   plugins: ["@babel/plugin-proposal-object-rest-spread"],
 
   env: {
