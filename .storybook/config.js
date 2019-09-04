202d3e07e97f6561d7fc519dc46d8b3623421a9b
Gert Hengeveld <info@ghengeveld.nl>
2019-09-04 23:03:59 +0200

    Setup Chromatic.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,6 @@
 import { configure } from "@storybook/react"
 
+import "storybook-chromatic"
+
 const req = require.context("../stories", true, /\.stories\.js$/)
 configure(() => req.keys().forEach(filename => req(filename)), module)
