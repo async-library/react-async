d70941637add2408a3cb773cb69032e46a1b1fa2
Lenz Weber <mail@lenzw.de>
2019-09-20 09:44:37 +0200

    wildcard-export all types

--- packages/react-async/src/index.ts
+++ packages/react-async/src/index.ts
@@ -1,28 +1,6 @@
 import Async from "./Async"
 export { default as Async, createInstance } from "./Async"
-export {
-  PromiseFn,
-  InitialChildren,
-  PendingChildren,
-  FulfilledChildren,
-  Start,
-  Cancel,
-  Reject,
-  AsyncProps,
-  RejectedChildren,
-  SettledChildren,
-  DeferFn,
-  Fulfill,
-  AsyncAction,
-  AbstractState,
-  AsyncInitial,
-  AsyncPending,
-  AsyncFulfilled,
-  AsyncChildren,
-  AsyncOptions,
-  AsyncRejected,
-  AsyncState,
-} from "./types"
+export * from "./types"
 export { default as useAsync, useFetch, FetchOptions } from "./useAsync"
 export default Async
 export { statusTypes } from "./status"
